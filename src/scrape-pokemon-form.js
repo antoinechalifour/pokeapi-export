9b3351e56c7749318d2e5a98b30558d2252ec1a8
antoinechalifour <antoine.chalifour@gmail.com>
2019-12-25 23:08:16 +0100

    fix: add missing pokemon forms

--- src/scrape-pokemon-form.js
+++ src/scrape-pokemon-form.js
@@ -2,7 +2,7 @@ const path = require("path");
 const { scrape } = require("./scrape");
 
 scrape({
-  from: 1,
+  from: 10000,
   outdir: path.join(__dirname, "../data/pokemon-form"),
   buildOutFile: (id, form) => `${id}-${form.name}.json`,
   buildUrl: id => `https://pokeapi.co/api/v2/pokemon-form/${id}`
